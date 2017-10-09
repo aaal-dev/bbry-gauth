31d53e45493a1cad574f4f5356197621333a1e5e
animeslave <ascii.animeslave@gmail.com>
2017-10-09 11:31:07 +0400

    Перестраиваю репозиторий

---
+++ translations/GoogleAuthenticator_ru.ts
@@ -0,0 +1,151 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="ru_RU">
+<context>
+    <name>AddCodePage</name>
+    <message>
+        <location filename="../assets/pages/AddCodePage.qml" line="11"/>
+        <source>Manual edit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../assets/pages/AddCodePage.qml" line="14"/>
+        <source>Create</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../assets/pages/AddCodePage.qml" line="27"/>
+        <source>Cancel</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../assets/pages/AddCodePage.qml" line="47"/>
+        <source>Enter web-service title, auth login and secret key from the 2-step authentication setup page</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../assets/pages/AddCodePage.qml" line="63"/>
+        <source>Title</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../assets/pages/AddCodePage.qml" line="79"/>
+        <source>Your login</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../assets/pages/AddCodePage.qml" line="100"/>
+        <source>Two factor authentication code</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../assets/pages/AddCodePage.qml" line="114"/>
+        <source></source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../assets/pages/AddCodePage.qml" line="156"/>
+        <source>Setup preferences for output key. If you don&apos;t know, leave it by default</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../assets/pages/AddCodePage.qml" line="167"/>
+        <source>Key Length:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../assets/pages/AddCodePage.qml" line="190"/>
+        <source>Type:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../assets/pages/AddCodePage.qml" line="194"/>
+        <location filename="../assets/pages/AddCodePage.qml" line="195"/>
+        <source>Time based OTP</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../assets/pages/AddCodePage.qml" line="199"/>
+        <location filename="../assets/pages/AddCodePage.qml" line="200"/>
+        <source>Counter based OTP</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>AddManualyPage</name>
+    <message>
+        <location filename="../assets/pages/AddManualyPage.qml" line="76"/>
+        <source></source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>EditPage</name>
+    <message>
+        <location filename="../assets/pages/EditPage.qml" line="82"/>
+        <source></source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>MainPage</name>
+    <message>
+        <location filename="../assets/pages/MainPage.qml" line="122"/>
+        <location filename="../assets/pages/MainPage.qml" line="155"/>
+        <source>Authenticator Code</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../assets/pages/MainPage.qml" line="127"/>
+        <location filename="../assets/pages/MainPage.qml" line="160"/>
+        <source>Delete This Entry</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../assets/pages/MainPage.qml" line="174"/>
+        <source>Scan QR</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../assets/pages/MainPage.qml" line="186"/>
+        <source>Add code</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ScanQRCodePage</name>
+    <message>
+        <location filename="../assets/pages/ScanQRCodePage.qml" line="33"/>
+        <source>Scan QR Code</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../assets/pages/ScanQRCodePage.qml" line="45"/>
+        <source>Move device until you get QR code on your visual site, then stand still until decoding</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SettingsPage</name>
+    <message>
+        <location filename="../assets/pages/SettingsPage.qml" line="8"/>
+        <source>Settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../assets/pages/SettingsPage.qml" line="13"/>
+        <source>Theme</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../assets/pages/SettingsPage.qml" line="54"/>
+        <source>Color</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../assets/pages/SettingsPage.qml" line="27"/>
+        <source>Dark theme</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
